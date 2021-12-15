c22fdb232c187b9743d88b7ab851f47ed9dc3601
keithcharlesc <keithcharleschan@gmail.com>
2021-12-15 16:41:44 +0800

    Die set-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
+import React from "react";
+import Die from "./components/Die";
+import "./App.css";
+
+export default function App() {
+  function randomDieNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+
   return (
-    <div className="app-border">
-      <main></main>
-    </div>
+    <main className="app-border">
+      <div className="container">
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+        <Die value={randomDieNumber()} />
+      </div>
+    </main>
   );
 }
-
-export default App;
---
+++ src/components/Die.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Die.css";
+
+export default function Die(props) {
+  return <div className="die">{props.value}</div>;
+}
