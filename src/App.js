ede63d569d24704661dcc71d9bf96158bc345547
keithcharlesc <keithcharleschan@gmail.com>
2021-12-15 18:34:30 +0800

    Roll button and mapping to components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import Die from "./components/Die";
 import "./App.css";
 
 export default function App() {
-  function randomDieNumber() {
-    return Math.floor(Math.random() * 6) + 1;
+  const [dices, setDices] = React.useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceElements = dices.map((die, index) => (
+    <Die key={index} value={die} />
+  ));
+
+  function handleClick() {
+    setDices(allNewDice());
   }
 
   return (
     <main className="app-border">
-      <div className="container">
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-        <Die value={randomDieNumber()} />
-      </div>
+      <div className="container">{diceElements}</div>
+      <button className="roll-button" onClick={handleClick}>
+        Roll
+      </button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./Die.css";
 
 export default function Die(props) {
-  return <div className="die">{props.value}</div>;
+  return (
+    <div className="die">
+      <p className="die-number">{props.value}</p>
+    </div>
+  );
 }
